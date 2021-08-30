44606b31682e7be6c12a05ae967e7b1f09d5a17e
sssshiki <sssshiki@oulook.com>
2021-08-31 00:33:58 +0800

    watch

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 new Vue({
     el:'#root',
     data:{
-        firstName:'张'
+        firstName:'张',
+        lastName:'三',
+        fullName:''
+    },
+    watch:{
+        firstName(newValue,oldValue){
+            this.fulltName = newValue + '-' + this.lastName
+        }
     }
 })
